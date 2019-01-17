8d03869be4dcb7f39533e68842202513fb8e6fc7
anagha <anaghav1998@gmail.com>
2019-01-17 10:22:18 +0530

    Substract added by team2

--- hello.java
+++ hello.java
@@ -7,8 +7,8 @@ public class hello {
 		char choice, ch;
 		Scanner scan = new Scanner (System.in);
 		do{
-			System.out.print("1. Add, 2. Multiply\n");
-			System.out.print("3. Exit\n");
+			System.out.print("1. Add, 2. Multiply 3. sub\n");
+			System.out.print("4. Exit\n");
 			System.out.print("enter your choice: ");
 			choice=scan.next().charAt(0);
 			switch(choice){
@@ -24,7 +24,13 @@ public class hello {
 						  res=a*b;
 						  System.out.print("Result = "+res);
 						  break;
-				case '3': System.exit(0);
+				case '3': System.out.print("enter 2 numbers: ");
+						  a=scan.nextFloat();
+						  b=scan.nextFloat();
+						  res=a-b;
+						  System.out.print("Result = "+res);
+						  break;
+				case '4': System.exit(0);
 						  break;
 				default : System.out.print("Invalid choice \n");
 						  break;
