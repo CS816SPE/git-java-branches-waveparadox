9963ee4c86d22c13fb2539db744964b135a39c9c
anagha <anaghav1998@gmail.com>
2019-01-17 10:12:01 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class hello {
+	public static void main (String args[]){
+		float a,b, res;
+		char choice, ch;
+		Scanner scan = new Scanner (System.in);
+		do{
+			System.out.print("1. Add\n");
+			System.out.print("2. Exit\n");
+			System.out.print("enter your choice: ");
+			choice=scan.next().charAt(0);
+			switch(choice){
+				case '1': System.out.print("enter 2 numbers: ");
+						  a=scan.nextFloat();
+						  b=scan.nextFloat();
+						  res=a+b;
+						  System.out.print("Result = "+res);
+						  break;
+				case '2': System.exit(0);
+						  break;
+				default : System.out.print("Invalid choice \n");
+						  break;
+
+			}
+			System.out.print("\n--------------");
+		}while(choice!=2);
+	}
+}
